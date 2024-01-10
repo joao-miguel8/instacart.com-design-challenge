e00d00b2a738ec93174001ea7ce0a0ee51cdfab6
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-10 13:39:01 -0500

    :sparkles: create header menu nav container mark and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import { RxHamburgerMenu } from "react-icons/rx";
+import Flag from "react-world-flags";
+import { IoChevronForward } from "react-icons/io5";
 import "./App.css";
-import "./styling/header.css";
-import InstaCartLogo from "./assets/nav-carrot-logo.webp";
-
+import Header from "./components/header/Header";
+import "./styling/nav.css";
 function App() {
+	const navList = [{ label: "Departments" }, { label: "More ways to shop" }, { label: "Help" }];
+
 	return (
-		<header className="header">
-			<nav className="header-nav">
-				<div className="nav-left">
-					<button className="nav-menu-button">
-						<RxHamburgerMenu size={"1.2rem"} />
+		<>
+			<Header />
+			<div className="nav-menu-container">
+				<Header />
+				<div className="nav">
+					<div className="nav-list">
+						{navList.map(item => (
+							<button key={item.label}>
+								<span>{item.label}</span>
+								<IoChevronForward size={".7rem"} color={"#C7C8CD"} />
+							</button>
+						))}
+					</div>
+					{/* country select */}
+					<button className="nav-change-country-btn">
+						<div className="nav-country">
+							<Flag code={"US"} />
+							<span>United States</span>
+						</div>
+						<span className="change-country">Change</span>
 					</button>
-					<img className="nav-left-instacart-logo" src={InstaCartLogo} alt="Carrot" />
 				</div>
-				<div className="nav-right">
-					<button>Log in</button>
-					<button>Sign up</button>
-				</div>
-			</nav>
-		</header>
+			</div>
+		</>
 	);
 }
 
