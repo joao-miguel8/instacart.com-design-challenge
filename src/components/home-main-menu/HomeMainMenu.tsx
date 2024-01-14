7e93504611a17663c55d9452190d609c88f3fb02
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-14 13:26:22 -0500

    Add: create AccountSignUpWindow.tsx mark up/styling
    - Add style folder and add accountSignUpWindow.css and homeMainMenu.css files
    - Add SignUpOptionsType
    - Add signUpOptions
    - Add signUpLogoIcons

--- src/components/home-main-menu/HomeMainMenu.tsx
+++ src/components/home-main-menu/HomeMainMenu.tsx
@@ -1,11 +1,15 @@
 import { IoChevronBack, IoChevronForward, IoClose } from "react-icons/io5";
 import { IoIosCheckmark } from "react-icons/io";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
+import { BsFillTelephoneOutboundFill } from "react-icons/bs";
 import Flag from "react-world-flags";
 import InstaCartLogo from "../../assets/nav-carrot-logo.webp";
 import InstaCartLogoLg from "../../assets/instacart-logo-lg.webp";
-import "./homeMainMenu.css";
+import "../home-main-menu/style/accountSignUpWindow.css";
+import "../home-main-menu/style/homeMainMenu.css";
 import "../../App.css";
-import { useState } from "react";
+import { useState, ReactElement } from "react";
 
 type CountrySelectionOptionsType = {
 	flag: string;
@@ -15,6 +19,11 @@ type CountrySelectionOptionsType = {
 type NavListType = {
 	label: string;
 	dropDownItems: string[];
+};
+
+type SignUpOptionsType = {
+	icon: ReactElement;
+	title: string;
 };
 
 function ChangeCountryPopup({
@@ -95,6 +104,52 @@ function SelectedMenuItemList({ menuItems, isMenuItemsListOpen, setIsMenuItemsLi
 	);
 }
 
+function AccountSignUpWindow({ signUpOptions }: { signUpOptions: SignUpOptionsType[] }) {
+	return (
+		<div className="sign-in-container">
+			<div className="sign-in-container-header">
+				<IoClose size="1.4rem" color={"#343538"} />
+				<h2>Sign up</h2>
+			</div>
+			<div className="sign-in-options-and-input-wrapper">
+				<div className="sign-in-options-container">
+					{signUpOptions.map(option => {
+						return (
+							<button>
+								<div className="sign-in-option-icons">{option.icon}</div>
+								<span>{option.title}</span>
+							</button>
+						);
+					})}
+				</div>
+				<div className="personal-sign-up-input-container">
+					<div className="sign-in-options-input-divider">or</div>
+					<p className="sign-in-prompt-msg">Enter your email to get started</p>
+					<div className="email-or-input-container">
+						<input required type="text" name="email" />
+						<label className="sign-up-email-or-input-label" htmlFor="email">
+							Email
+						</label>
+					</div>
+					<p className="terms-and-conditions-signUp">
+						By continuing, you agree to our
+						<a href="#">Terms of Service</a> & <a href="#">Privacy Policy</a>.
+					</p>
+					<button className="signUp-continue-btn">
+						<span>Continue</span>
+					</button>
+				</div>
+			</div>
+			<div className="signUp-log-in-container">
+				<p>Already have an account?</p>
+				<button>
+					<span>Log in</span>
+				</button>
+			</div>
+		</div>
+	);
+}
+
 function HomeMainMenu({ handleCloseNavMenu, isMenuOpen }: { handleCloseNavMenu: () => void; isMenuOpen: boolean }) {
 	const navList: NavListType[] = [
 		{ label: "Departments", dropDownItems: ["Food", "Pantry", "Seafood", "Baked goods", "Dairy", "Fresh produce", "Deli"] },
@@ -105,6 +160,16 @@ function HomeMainMenu({ handleCloseNavMenu, isMenuOpen }: { handleCloseNavMenu: 
 	const countrySelection: CountrySelectionOptionsType[] = [
 		{ flag: "US", name: "United States" },
 		{ flag: "CA", name: "Canada" },
+	];
+
+	const signUpLogoIcons = {
+		size: "1.6rem",
+	};
+
+	const signUpOptions: SignUpOptionsType[] = [
+		{ icon: <FcGoogle size={signUpLogoIcons.size} />, title: "Continue with Google" },
+		{ icon: <FaFacebook size={signUpLogoIcons.size} color={"#4267B2"} />, title: "Continue with Facebook" },
+		{ icon: <BsFillTelephoneOutboundFill size={signUpLogoIcons.size} />, title: "Continue with Phone" },
 	];
 
 	const [isMenuItemsListOpen, setIsMenuItemsListOpen] = useState(false);
@@ -164,6 +229,8 @@ function HomeMainMenu({ handleCloseNavMenu, isMenuOpen }: { handleCloseNavMenu: 
 					<span className="change-country">Change</span>
 				</button>
 				<ChangeCountryPopup isCloseCountryChoicePopupOpen={isCloseCountryChoicePopupOpen} setIsCloseCountryChoicePopupOpen={setIsCloseCountryChoicePopupOpen} countryOptions={countrySelection} countrySelected={countrySelected} setCountrySelected={setCountrySelected} />
+				{/* <<<<<<<< SIGN IN POPUP >>>>>>> */}
+				<AccountSignUpWindow signUpOptions={signUpOptions} />
 			</div>
 			{/* MENU OPEN OVERLAY */}
 			{isMenuOpen && <div onClick={handleCloseNavMenu} className="nav-menu-open-overlay"></div>}
