494297e91ea484166193b9485d1400bf25bd9f8f
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-15 22:57:50 -0500

    Add: toggleLoginOption state for Login Toggle functionality and css
    - Add condition for disabling email and phone number input
    - Add conditional logic for content that needs toggleLoginOption condition
    - Add css for login form

--- src/components/account-signup-window/AccountSignUpWindow.tsx
+++ src/components/account-signup-window/AccountSignUpWindow.tsx
@@ -14,6 +14,8 @@ export function AccountSignUpWindow({ loginSignUpStatus, setLoginSignUpStatus }:
 		size: "1.6rem",
 	};
 
+	const [toggleLoginOption, setToggleLoginOption] = useState(false);
+
 	const [signUpInputs, setSignUpInputs] = useState({
 		emailInput: "",
 		phoneInput: "",
@@ -50,7 +52,7 @@ export function AccountSignUpWindow({ loginSignUpStatus, setLoginSignUpStatus }:
 					<button onClick={() => setLoginSignUpStatus({ ...loginSignUpStatus, isSignUpEnabled: false })}>
 						<IoClose size="1.4rem" color={"#343538"} />
 					</button>
-					<h2>Sign up</h2>
+					{toggleLoginOption ? <h2>Sign up</h2> : <h2>Log in</h2>}
 				</div>
 				<div className="sign-in-options-and-input-wrapper">
 					<div className="sign-in-options-container">
@@ -74,43 +76,102 @@ export function AccountSignUpWindow({ loginSignUpStatus, setLoginSignUpStatus }:
 								</button>
 							))}
 					</div>
+
+					{/* -----------------SIGN-UP SECTION----------------- */}
 					<div className="personal-sign-up-input-container">
 						<div className="sign-in-options-input-divider">or</div>
-						{showEmail === true ? (
+						<>
+							{toggleLoginOption !== false && (
+								<>
+									{showEmail === true ? (
+										<>
+											<p className="sign-in-prompt-msg">Enter your Phone number to get started</p>
+											<div className="email-or-input-container">
+												<input onChange={e => setSignUpInputs({ ...signUpInputs, phoneInput: e.target.value })} required type="tel" name="phoneNumber" value={signUpInputs.phoneInput} />
+												<label className="sign-up-email-or-input-label" htmlFor="phoneNumber">
+													Phone Number
+												</label>
+											</div>
+											<p className="terms-and-conditions-phone-signUp">
+												We will send a text with a verification code. Message and data rates may apply. By continuing, you agree to our <a href="#">Terms of Service</a> & <a href="#">Privacy Policy</a>.
+											</p>
+											<button className="signUp-continue-btn">
+												<span>Continue</span>
+											</button>
+										</>
+									) : (
+										<>
+											<p className="sign-in-prompt-msg">Enter your Email to get started</p>
+											<div className="email-or-input-container">
+												<input onChange={e => setSignUpInputs({ ...signUpInputs, emailInput: e.target.value })} required type="text" name="email" value={signUpInputs.emailInput} />
+												<label className="sign-up-email-or-input-label" htmlFor="email">
+													Email
+												</label>
+												<p className="terms-and-conditions-email-signUp">
+													By continuing, you agree to our
+													<a href="#">Terms of Service</a> & <a href="#">Privacy Policy</a>.
+												</p>
+												<button className="signUp-continue-btn">
+													<span>Continue</span>
+												</button>
+											</div>
+										</>
+									)}
+								</>
+							)}
+						</>
+
+						{/* -----------------LOGIN SECTION----------------- */}
+						{!toggleLoginOption && (
 							<>
-								<p className="sign-in-prompt-msg">Enter your Phone number to get started</p>
-								<div className="email-or-input-container">
-									<input onChange={e => setSignUpInputs({ ...signUpInputs, phoneInput: e.target.value })} required type="tel" name="phoneNumber" value={signUpInputs.phoneInput} />
-									<label className="sign-up-email-or-input-label" htmlFor="phoneNumber">
-										Phone Number
-									</label>
-								</div>
-							</>
-						) : (
-							<>
-								<p className="sign-in-prompt-msg">Enter your Email to get started</p>
-								<div className="email-or-input-container">
-									<input onChange={e => setSignUpInputs({ ...signUpInputs, emailInput: e.target.value })} required type="text" name="email" value={signUpInputs.emailInput} />
-									<label className="sign-up-email-or-input-label" htmlFor="email">
-										Email
-									</label>
-								</div>
+								{showEmail ? (
+									<div>
+										<p className="sign-in-prompt-msg">Enter your Phone number to get started</p>
+										<div className="email-or-input-container">
+											<input onChange={e => setSignUpInputs({ ...signUpInputs, phoneInput: e.target.value })} required type="tel" name="phoneNumber" value={signUpInputs.phoneInput} />
+											<label className="sign-up-email-or-input-label" htmlFor="phoneNumber">
+												Phone Number
+											</label>
+										</div>
+										<p className="terms-and-conditions-phone-signUp">
+											We will send a text with a verification code. Message and data rates may apply. By continuing, you agree to our <a href="#">Terms of Service</a> & <a href="#">Privacy Policy</a>.
+										</p>
+										<button className="signUp-continue-btn">
+											<span>Continue</span>
+										</button>
+									</div>
+								) : (
+									<div className="login-inputs-container">
+										<div className="login-email-input-container">
+											<input required type="text" name="email" />
+											<label className="login-email-label-input" htmlFor="email">
+												Email
+											</label>
+										</div>
+										<div className="login-password-input-container">
+											<input required type="text" name="password" />
+											<label className="login-password-label-input" htmlFor="password">
+												Password
+											</label>
+										</div>
+										<div className="forgot-password-container">
+											<p>Forgot password?</p>
+											<button>
+												<span>Reset it</span>
+											</button>
+										</div>
+										<button className="login-btn">
+											<span>Log in</span>
+										</button>
+									</div>
+								)}
 							</>
 						)}
-						<p className="terms-and-conditions-signUp">
-							By continuing, you agree to our
-							<a href="#">Terms of Service</a> & <a href="#">Privacy Policy</a>.
-						</p>
-						<button className="signUp-continue-btn">
-							<span>Continue</span>
-						</button>
 					</div>
 				</div>
 				<div className="signUp-log-in-container">
-					<p>Already have an account?</p>
-					<button>
-						<span>Log in</span>
-					</button>
+					{toggleLoginOption ? <p>Already have an account?</p> : <p>Don't have an account?</p>}
+					<button onClick={() => setToggleLoginOption(prevVal => !prevVal)}>{toggleLoginOption ? <span>Log in</span> : <span>Sign up</span>}</button>
 				</div>
 			</div>
 			{loginSignUpStatus.isSignUpEnabled && <div className="account-signUp-window-overlay"></div>}
