52eb184294badd1f8ec9c9fee1980a2f3e5a213c
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-17 12:05:05 -0500

    - Add QRcode call to action section styling/markup/media queries and image import
    - Add homepage-qrcode-img image import
    - Add qrcode-phone-img image import
    - Add qr code call to action section styling in homepage.css
    - Add markup for qr code call to action section and image imports in Homepage.tsx

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Header from "../../components/header/Header";
+import "./homepage.css";
+import QRCodeIMG from "../../assets/homepage-qrcode-img.png";
+import QRCodePhoneIMG from "../../assets/qrcode-phone-img.webp";
 import { AccountSignUpWindow } from "../../components/account-signup-window/AccountSignUpWindow";
 import type { LoginSignUpStatusType } from "../../components/account-signup-window/types/LoginSignUpStatusType";
-import "./homepage.css";
 
 function Homepage() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -19,11 +21,21 @@ function Homepage() {
 			<Header handleCloseMainMenu={() => handleCloseNavMenu()} isMenuOpen={isMenuOpen} loginSignUpStatus={loginSignUpStatus} setLoginSignUpStatus={setLoginSignUpStatus} />
 			{loginSignUpStatus.isSignUpEnabled && <AccountSignUpWindow loginSignUpStatus={loginSignUpStatus} setLoginSignUpStatus={setLoginSignUpStatus} />}
 
+			{/* ----- HERO SECTION ------ */}
 			<div className={`bg-hero-img`}>
 				<div>
 					<h1>Order groceries for delivery or pickup today</h1>
 					<p>Whatever you want from local stores, brought right to your door.</p>
 				</div>
+			</div>
+			{/* ----- QRCode CTA SECTION ------ */}
+			<div className="qr-code-cta-container">
+				<img src={QRCodePhoneIMG} alt="Himg" />
+				<div>
+					<h2>Get the full Instacart experience</h2>
+					<p>Scan the QR code with your camera. First delivery is free.</p>
+				</div>
+				<img src={QRCodeIMG} alt="QR code img" className="qr-code-cta-img" />
 			</div>
 		</>
 	);
