56e573c9dbb6d3d08d034bc6192b5f59e9ac3cee
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-19 22:22:23 -0500

    - Add css stlying to BecomeAShopperServicesAccordion.tsx.tsx
    - Add props to BecomeAShopperServicesAccordion.tsx
    - Remove css selector from getDeliveriesServicesAccordion.css

--- src/pages/homepage/components/become-a-shopper-services/BecomeAShopperServicesAccordion.tsx
+++ src/pages/homepage/components/become-a-shopper-services/BecomeAShopperServicesAccordion.tsx
@@ -3,15 +3,23 @@ import { IoChevronDown } from "react-icons/io5";
 import Accordion from "../../../../components/accordion/Accordion";
 import AccordionItem from "../../../../components/accordion/AccordionItem";
 import { becomeAShopperServicesData } from "./becomeAShopperServicesData";
+import "../become-a-shopper-services/style/becomeAShopperServices.css";
 
 function BecomeAShopperServicesAccordion() {
 	return (
 		<>
-			<Accordion accordionWrapperStyling="accordion-wrapper">
+			<Accordion accordionWrapperStyling="become-a-shopper-services-accordion-wrapper-accordion-wrapper">
 				{becomeAShopperServicesData.map(item => {
 					return (
-						<AccordionItem key={item.title} iconClosed={<IoChevronDown size={"1rem"} />} iconOpened={<IoChevronUp size={"1rem"} />} accordionTitleContainer={<h3>{item.title}</h3>}>
-							<ul className="accordion-list-items">
+						<AccordionItem
+							key={item.title}
+							iconClosed={<IoChevronDown size={"1rem"} />}
+							iconOpened={<IoChevronUp size={"1rem"} />}
+							accordionItemWrapper={"become-a-shopper-services-accordion-item-wrapper"}
+							accordionTitleContainerStyling="become-a-shopper-services-accordion-title-container"
+							accordionContentContainerStyling="become-a-shopper-services-accordion-content-container"
+							accordionTitleContainer={<h3>{item.title}</h3>}>
+							<ul className="become-a-shopper-services-accordion-list-items">
 								{item.links.map(linkItem => {
 									return (
 										<li key={linkItem.title}>
