3b708d6e349a10d1ea210dcca75d5f2ce6c2e332
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-21 18:18:47 -0500

    - Add search list container for Searchbar.tsx
    - Add isSearchFocused state variable
    - Add popularSearchesData
    - Add onBlue and onFocus to check if input is focused
    - Add styling and markup for search list container

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -2,9 +2,21 @@ import { useRef, useState } from "react";
 import "./style/searchbar.css";
 import { BiSearch } from "react-icons/bi";
 import { IoMdClose } from "react-icons/io";
+import eggThumbNailImg from "../../assets/eggs-thumbnail-img.jpeg";
+
+const popularSearchesData = [
+	{ itemName: "egg", img: eggThumbNailImg },
+	{ itemName: "egg", img: eggThumbNailImg },
+	{ itemName: "egg eggs and eggs", img: eggThumbNailImg },
+	{ itemName: "egg", img: eggThumbNailImg },
+	{ itemName: "egg", img: eggThumbNailImg },
+	{ itemName: "egg", img: eggThumbNailImg },
+	{ itemName: "egg", img: eggThumbNailImg },
+];
 
 function Searchbar() {
 	const [searchQuery, setSearchQuery] = useState("");
+	const [isSearchFocused, setIsSearchFocused] = useState(false);
 	const inputRef = useRef<HTMLInputElement | null>(null);
 
 	const handleSearchReset = () => {
@@ -15,15 +27,39 @@ function Searchbar() {
 	};
 
 	return (
-		<div className="search-bar-container">
-			<BiSearch size={"1.6rem"} />
-			<input ref={inputRef} onChange={e => setSearchQuery(e.target.value)} value={searchQuery} placeholder="Search products and stores" type="text" />
-			{searchQuery && (
-				<button onClick={() => handleSearchReset()} className="clear-search-btn">
-					<IoMdClose size={"1.6rem"} />
-				</button>
-			)}
-		</div>
+		<>
+			<div className="search-bar-container">
+				<BiSearch size={"1.6rem"} />
+				<input ref={inputRef} onBlur={() => setIsSearchFocused(false)} onFocus={() => setIsSearchFocused(true)} onChange={e => setSearchQuery(e.target.value)} value={searchQuery} placeholder="Search products and stores" type="text" />
+				{searchQuery && (
+					<button onClick={() => handleSearchReset()} className="clear-search-btn">
+						<IoMdClose size={"1.6rem"} />
+					</button>
+				)}
+				{/* ------ SEARCH LIST CONTAINER ---------- */}
+				{isSearchFocused && !searchQuery ? (
+					<div className="popular-searches-list-wrapper">
+						<h4>Popular Searches</h4>
+						<div className="popular-searches-list-container">
+							<ul>
+								{popularSearchesData.map(item => {
+									return (
+										<li className="popular-search-list-item">
+											<a>
+												<img src={item.img} alt="img" />
+												<span>{item.itemName}</span>
+											</a>
+										</li>
+									);
+								})}
+							</ul>
+						</div>
+					</div>
+				) : (
+					<div className="search-list-container"></div>
+				)}
+			</div>
+		</>
 	);
 }
 
