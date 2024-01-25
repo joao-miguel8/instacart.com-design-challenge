064c7051c40f1b0faf77dbe3666f7cae24f5df0e
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-25 17:31:19 -0500

    Add: Create HeroSection.tsx component
    - Rename homepage-qrcode-img

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 import "./homepage.css";
 import "../../components/accordion/style.css";
-import { FaApple } from "react-icons/fa";
-import { BiLogoPlayStore } from "react-icons/bi";
-
-import instacartLogo from "../../assets/nav-carrot-logo.webp";
-import QRCodeIMG from "../../assets/homepage-qrcode-img.png";
-import QRCodePhoneIMG from "../../assets/qrcode-phone-img.webp";
 import benefit01 from "../../assets/grocery-delivery-benefit-img.webp";
 import benefit02 from "../../assets/grocery-delivery-see-real-time-updates-benefit.webp";
 import benefit03 from "../../assets/grocery-delivery-get-your-items-same-day-benefit-img.webp";
@@ -16,15 +10,13 @@ import Header from "../../components/header/Header";
 import GroceryDeliveryCard from "./components/grocery-delivery-card/GroceryDeliveryCard";
 import StatisticFactsAccordion from "./components/statistic-facts-accordion/StatisticFactsAccordion";
 import CommonQuestionsAccordion from "./components/common-questions-accordion/CommonQuestionsAccordion";
-import GetDeliveriesServicesAccordion from "./components/get-deliveries-services-accordion/GetDeliveriesServicesAccordion";
 import type { LoginSignUpStatusType } from "../../components/account-signup-window/types/LoginSignUpStatusType";
 import type { GroceryCardType } from "./components/grocery-delivery-card/types/GroceryCardType";
 import useMediaQuery from "../../hooks/useMediaQuery";
-import DeliveryServicesList from "./components/get-deliveries-services-accordion/components/DeliveryServicesList";
-
 import Footer from "../../components/footer/Footer";
 import BecomeAShopperServices from "../../components/footer/components/BecomeAShopperServices";
 import GetDeliveryServices from "../../components/footer/components/GetDeliveryServices";
+import HeroSection from "./components/hero-section/HeroSection";
 
 function Homepage() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -49,22 +41,7 @@ function Homepage() {
 			{loginSignUpStatus.isSignUpEnabled && <AccountSignUpWindow loginSignUpStatus={loginSignUpStatus} setLoginSignUpStatus={setLoginSignUpStatus} />}
 
 			{/* ----- HERO SECTION ------ */}
-			<div className={`bg-hero-img`}>
-				<div>
-					<h1>Order groceries for delivery or pickup today</h1>
-					<p>Whatever you want from local stores, brought right to your door.</p>
-				</div>
-			</div>
-			{/* ----- QRCode CTA SECTION ------ */}
-			<div className="qr-code-cta-container">
-				<img src={QRCodePhoneIMG} alt="Himg" />
-				<div>
-					<h2>Get the full Instacart experience</h2>
-					<p>Scan the QR code with your camera. First delivery is free.</p>
-				</div>
-				<img src={QRCodeIMG} alt="QR code img" className="qr-code-cta-img" />
-			</div>
-
+			<HeroSection />
 			{/* ----- GROCERY DELIVERY BENEFIT SECTION ------ */}
 			<div className="grocery-delivery-benefits-container">
 				<h2>Grocery delivery you can count on</h2>
---
+++ src/pages/homepage/components/hero-section/HeroSection.tsx
@@ -0,0 +1,26 @@
+import QRCodePhoneIMG from "../../../../assets/qrcode-phone-img.webp";
+import QRCodeIMG from "../../../../assets/qrcode-img.png";
+
+function HeroSection() {
+	return (
+		<section>
+			<div className={`bg-hero-img`}>
+				<div>
+					<h1>Order groceries for delivery or pickup today</h1>
+					<p>Whatever you want from local stores, brought right to your door.</p>
+				</div>
+			</div>
+			{/* ----- QRCode CTA SECTION ------ */}
+			<div className="qr-code-cta-container">
+				<img src={QRCodePhoneIMG} alt="Himg" />
+				<div>
+					<h2>Get the full Instacart experience</h2>
+					<p>Scan the QR code with your camera. First delivery is free.</p>
+				</div>
+				<img src={QRCodeIMG} alt="QR code img" className="qr-code-cta-img" />
+			</div>
+		</section>
+	);
+}
+
+export default HeroSection;
