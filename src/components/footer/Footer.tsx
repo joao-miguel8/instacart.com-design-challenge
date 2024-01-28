7f11e60771d720c85975c7afc2d1f03aafebd7f4
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-28 17:41:18 -0500

    - Change padding for each footer section and change text color for footer headings

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import "./style/footer.css";
 function Footer({ children }: { children: ReactNode }) {
-	return <footer>{children}</footer>;
+	return <footer className="footer-wrapper">{children}</footer>;
 }
 
 export default Footer;
